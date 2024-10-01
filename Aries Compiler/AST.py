80e98e75fc9aed41c478c8af0ccc9944a0c612aa
OrionCummings <orioncummings23@gmail.com>
2024-09-30 20:31:37 -0400

    Created AST classes

---
+++ Aries Compiler/AST.py
@@ -0,0 +1,22 @@
+
+class AST():
+    """An Abstract Syntax Tree."""
+    
+    def __init__(self) -> None:
+        pass
+
+class ASTNode(AST):
+    """An AST node. A base class for expression and statement nodes."""
+
+    def __init__(self) -> None:
+        pass
+
+class ASTExpressionNode(AST):
+
+    def __init__(self) -> None:
+        pass
+
+class ASTStatementNode(AST):
+
+    def __init__(self) -> None:
+        pass
