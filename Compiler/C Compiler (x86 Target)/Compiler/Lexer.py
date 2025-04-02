9fb66a3e6c6b20b9f1814df54f8a50387037f72a
Orion Cummings <orioncummings23@gmail.com>
2025-04-01 23:46:45 -0400

    reworked tokenization; string and char literals do not work at all

--- Compiler/C Compiler (x86 Target)/Compiler/Lexer.py
+++ Compiler/C Compiler (x86 Target)/Compiler/Lexer.py
@@ -7,19 +7,31 @@ class TokenType(Enum):
     NONE                    = auto(),
     EXIT                    = auto(),
     IDENTIFIER              = auto(),
+    LITERAL                 = auto(),
     EOF                     = auto(),
-    ARROW                   = auto(), # ->
-    PUNC_SEMICOLON          = auto(), # ;
-    PUNC_COLON              = auto(), # :
-    PUNC_COMMA              = auto(), # ,
-    PUNC_QUESTION           = auto(), # ?
-    PUNC_FSLASH             = auto(), # /
-    PUNC_PAREN_OPEN         = auto(), # (
-    PUNC_PAREN_CLOSE        = auto(), # )
-    PUNC_BRACKET_OPEN       = auto(), # [
-    PUNC_BRACKET_CLOSE      = auto(), # ]
-    PUNC_BRACE_OPEN         = auto(), # {
-    PUNC_BRACE_CLOSE        = auto(), # }
+    SYM_SEMICOLON           = auto(), # ;
+    SYM_COLON               = auto(), # :
+    SYM_COMMA               = auto(), # ,
+    SYM_QUESTION            = auto(), # ?
+    SYM_FSLASH              = auto(), # /
+    SYM_PAREN_OPEN          = auto(), # (
+    SYM_PAREN_CLOSE         = auto(), # )
+    SYM_BRACKET_OPEN        = auto(), # [
+    SYM_BRACKET_CLOSE       = auto(), # ]
+    SYM_BRACE_OPEN          = auto(), # {
+    SYM_BRACE_CLOSE         = auto(), # }
+    SYM_EQUAL               = auto(), # =
+    SYM_PLUS                = auto(), # +
+    SYM_DASH                = auto(), # -
+    SYM_SLASH               = auto(), # /
+    SYM_STAR                = auto(), # *
+    SYM_PERCENT             = auto(), # %
+    SYM_EXCLAIM             = auto(), # !
+    SYM_LT                  = auto(), # <
+    SYM_GT                  = auto(), # >
+    SYM_AMPERSAND           = auto(), # &
+    SYM_PIPE                = auto(), # |
+    SYM_CARRET              = auto(), # ^
     KEYWORD_IF              = auto(), # if
     KEYWORD_ELIF            = auto(), # elif
     KEYWORD_ELSE            = auto(), # else
@@ -40,46 +52,17 @@ class TokenType(Enum):
     KEYWORD_STRUCT          = auto(), # struct
     KEYWORD_SIZEOF          = auto(), # sizeof
     KEYWORD_TYPEOF          = auto(), # typeof
-    KEYWORD_STD             = auto(), # std
     KEYWORD_CAST            = auto(), # as
     KEYWORD_ASM             = auto(), # asm
     KEYWORD_NEW             = auto(), # new
     KEYWORD_TRUE            = auto(), # true
     KEYWORD_FALSE           = auto(), # false
-    OP_PLUS                 = auto(), # +
-    OP_DASH                 = auto(), # -
-    OP_SLASH                = auto(), # /
-    OP_STAR                 = auto(), # *
-    OP_PERCENT              = auto(), # %
-    OP_EQUIVALENCE          = auto(), # ==
-    OP_NONEQUIVALENCE       = auto(), # !=
-    OP_NOT                  = auto(), # !
-    OP_LT                   = auto(), # <
-    OP_LTE                  = auto(), # <=
-    OP_GT                   = auto(), # >
-    OP_GTE                  = auto(), # >=
-    OP_AND                  = auto(), # &&
-    OP_OR                   = auto(), # ||
-    OP_BIT_AND              = auto(), # &
-    OP_BIT_OR               = auto(), # |
-    OP_BIT_XOR              = auto(), # ^
-    OP_INC                  = auto(), # ++
-    OP_DEC                  = auto(), # --
-    OP_ADD_ASSIGN           = auto(), # +=
-    OP_SUB_ASSIGN           = auto(), # -=
-    OP_MUL_ASSIGN           = auto(), # *=
-    OP_DIV_ASSIGN           = auto(), # /=
-    OP_MOD_ASSIGN           = auto(), # %=
-    OP_ASSIGNMENT           = auto(), # =
-    OP_SL                   = auto(), # <<
-    OP_SR                   = auto(), # >>
-    OP_RL                   = auto(), # <<<
-    OP_RR                   = auto(), # >>>
-    TYPE_INT                = auto(), # int
-    TYPE_CHAR               = auto(), # char
-    TYPE_STRING             = auto(), # string
-    TYPE_BOOL               = auto(), # bool
-    TYPE_FLOAT              = auto(), # float
+    KEYWORD_INT             = auto(), # int
+    KEYWORD_UINT            = auto(), # uint
+    KEYWORD_FLOAT           = auto(), # float
+    KEYWORD_CHAR            = auto(), # char
+    KEYWORD_STRING          = auto(), # string
+    KEYWORD_VOID            = auto(), # void
 
     def __str__(type) -> str:
 
@@ -88,7 +71,7 @@ class TokenType(Enum):
         else:
             return "unknown"
 
-keywordDictionary: Dict[str, TokenType] = {
+keyword_dictionary: Dict[str, TokenType] = {
     "if":           TokenType.KEYWORD_IF,
     "while":        TokenType.KEYWORD_WHILE,
     "for":          TokenType.KEYWORD_FOR,
@@ -98,85 +81,167 @@ keywordDictionary: Dict[str, TokenType] = {
     "case":         TokenType.KEYWORD_CASE,
     "break":        TokenType.KEYWORD_BREAK,
     "default":      TokenType.KEYWORD_DEFAULT,
-    
     "def":          TokenType.KEYWORD_DEF,
     "struct":       TokenType.KEYWORD_STRUCT,
     "enum":         TokenType.KEYWORD_ENUM,
     "overload":     TokenType.KEYWORD_OVERLOAD,
     "const":        TokenType.KEYWORD_CONST,
     "static":       TokenType.KEYWORD_STATIC,
-    
     "return":       TokenType.KEYWORD_RETURN,
-    
     "sizeof":       TokenType.KEYWORD_SIZEOF,
     "typeof":       TokenType.KEYWORD_TYPEOF,
-    
     "as":           TokenType.KEYWORD_CAST,
-    
     "asm":          TokenType.KEYWORD_ASM,
-    
     "new":          TokenType.KEYWORD_NEW,
-    
     "true":         TokenType.KEYWORD_TRUE,
     "false":        TokenType.KEYWORD_FALSE,
+    "int":          TokenType.KEYWORD_INT,
+    "uint":         TokenType.KEYWORD_UINT,
+    "float":        TokenType.KEYWORD_FLOAT,
+    "char":         TokenType.KEYWORD_CHAR,
+    "string":       TokenType.KEYWORD_STRING,
+    "void":         TokenType.KEYWORD_VOID,
 }
 
-puncutationDictionary: Dict[str, TokenType] = {
-    ";": TokenType.PUNC_SEMICOLON,
-    ":": TokenType.PUNC_COLON,
-    
-    ",": TokenType.PUNC_COMMA,
-    
-    "?": TokenType.PUNC_QUESTION,
-    
-    "/": TokenType.PUNC_FSLASH,
-    
-    "(": TokenType.PUNC_PAREN_OPEN,
-    ")": TokenType.PUNC_PAREN_CLOSE,
-    
-    "{": TokenType.PUNC_BRACE_OPEN,
-    "}": TokenType.PUNC_BRACE_CLOSE,
-    
-    "[": TokenType.PUNC_BRACKET_OPEN,
-    "]": TokenType.PUNC_BRACKET_CLOSE,
+symbol_dictionary: Dict[str, TokenType] = {
+    ";": TokenType.SYM_SEMICOLON,
+    ":": TokenType.SYM_COLON,
+    ",": TokenType.SYM_COMMA,
+    "?": TokenType.SYM_QUESTION,
+    "(": TokenType.SYM_PAREN_OPEN,
+    ")": TokenType.SYM_PAREN_CLOSE,
+    "{": TokenType.SYM_BRACE_OPEN,
+    "}": TokenType.SYM_BRACE_CLOSE,
+    "[": TokenType.SYM_BRACKET_OPEN,
+    "]": TokenType.SYM_BRACKET_CLOSE,
+    "=": TokenType.SYM_EQUAL,
+    "+": TokenType.SYM_PLUS,
+    "-": TokenType.SYM_DASH,
+    "/": TokenType.SYM_SLASH,
+    "*": TokenType.SYM_STAR,
+    "%": TokenType.SYM_PERCENT,
+    "!": TokenType.SYM_EXCLAIM,
+    "<": TokenType.SYM_LT,
+    ">": TokenType.SYM_GT,
+    "&": TokenType.SYM_AMPERSAND,
+    "|": TokenType.SYM_PIPE,
+    "^": TokenType.SYM_CARRET,
 }
 
-operatorDictionary: Dict[str, TokenType] = {
-    "+":            TokenType.OP_PLUS,
-    "-":            TokenType.OP_DASH,
-    "/":            TokenType.OP_SLASH,
-    "*":            TokenType.OP_STAR,
-    "%":            TokenType.OP_PERCENT,
+pair_symbol_list = [
+    "\'",
+    "\"",
+]
+
+def is_keyword(sequence: str) -> Optional[TokenType]:
+    return keyword_dictionary[sequence] if sequence in keyword_dictionary else None
+
+def is_symbol(sequence: str) -> Optional[TokenType]:
+    return symbol_dictionary[sequence] if sequence in symbol_dictionary else None
+
+def is_identifier(sequence: str) -> Optional[TokenType]:
+
+        # A valid identifier is a sequence of characters 
+        # that begins with an alphabetical character and 
+        # contains any alphanumeric or valid symbolic character.
+        # Valid identifiers match with the following regex:
+        # \b[A-Za-z_][A-Za-z0-9_]*
+        # ^    ^         ^       ^
+        # |    |         |       |
+        # | begins with an alphabetical character
+        # |              |       |
+        # |        followed by any alphanumeric or valid symbolic character...
+        # must be on a           |
+        # word boundary   ...repeated any number of times
+        #                     (including zero times!)
+        #
+        # With this being said, regex is not used here (or anywhere in
+        # this lexer) because it is overkill. We will just follow
+        # python's default identifer check
+
+        return TokenType.IDENTIFIER if sequence.isidentifier() else None
+
+def is_literal(sequence: str) -> Optional[TokenType]:
+
+    if sequence == '': return None
+
+    if is_literal_integer_token_type := is_literal_integer(sequence):
+        return is_literal_integer_token_type
+    if is_literal_float_token_type   :=  is_literal_float(sequence):
+        return is_literal_float_token_type
+    if is_literal_char_token_type    :=  is_literal_char(sequence):
+        return is_literal_char_token_type
+    if is_literal_string_token_type  :=  is_literal_string(sequence):
+        return is_literal_string_token_type
+    if is_literal_bool_token_type    :=  is_literal_bool(sequence):
+        return is_literal_bool_token_type
     
-    "=":            TokenType.OP_ASSIGNMENT,
+    return None
+
+def is_literal_integer(sequence: str) -> Optional[TokenType]:
+
+    rules = [
+
+        # Must be numeric
+        sequence.isnumeric()
+    ]
+
+    return TokenType.LITERAL if all(rules) else None
+
+def is_literal_float(sequence: str) -> Optional[TokenType]:
+
+    rules = [
+
+        # Must end with 'f'
+        sequence[-1] == 'f', 
+
+        # If one f and one period are replaced, the number must be numeric
+        sequence.replace('f', '', count=1).replace('.', '', count=1).isnumeric()
+    ]
     
-    "==":           TokenType.OP_EQUIVALENCE,
-    "!=":           TokenType.OP_NONEQUIVALENCE,
-    "!":            TokenType.OP_NOT,
-    "<":            TokenType.OP_LT,
-    "<=":           TokenType.OP_LTE,
-    ">":            TokenType.OP_GT,
-    ">=":           TokenType.OP_GTE,
-    "&&":           TokenType.OP_AND,
-    "||":           TokenType.OP_OR,
+    return TokenType.LITERAL if any(rules) else None
+
+def is_literal_char(sequence: str) -> Optional[TokenType]:
+
+    # TODO: This will fail because of a lack of short circuit evaluation!
+    # What if len(sequence) < 3?
+    rules = [
+
+        # Must be three characters total (' x ')
+        len(sequence) == 3,
+
+        # Must start and end with single quotes
+        sequence[0] == "\'" and sequence[2] == "\'",
+
+        # The center symbol must be a valid character
+        sequence[1].isalpha()
+    ]
+
+    return TokenType.LITERAL if all(rules) else None
+
+def is_literal_string(sequence: str) -> Optional[TokenType]:
+
+    rules = [
+
+        # Must start and end with double quotes
+        sequence[0] == "\"" and sequence[-1] == "\"" 
+    ]
+
+    return TokenType.LITERAL if all(rules) else None
     
-    "&":            TokenType.OP_BIT_AND,
-    "|":            TokenType.OP_BIT_OR,
-    "^":            TokenType.OP_BIT_XOR,
-    "<<":           TokenType.OP_SL,
-    ">>":           TokenType.OP_SR,
-    "<<<":          TokenType.OP_RL,
-    ">>>":          TokenType.OP_RR,
+def is_literal_bool(sequence: str) -> Optional[TokenType]:
+
+    rules = [
+        sequence == "0",
+        sequence == "1",
+        sequence == "true",
+        sequence == "false",
+    ]
     
-    "+=":           TokenType.OP_ADD_ASSIGN,
-    "-=":           TokenType.OP_SUB_ASSIGN,
-    "*=":           TokenType.OP_MUL_ASSIGN,
-    "/=":           TokenType.OP_DIV_ASSIGN,
-    "%=":           TokenType.OP_MOD_ASSIGN,
-    
-    "++":           TokenType.OP_INC,
-    "--":           TokenType.OP_DEC,
-}
+    return TokenType.LITERAL if any(rules) else None
+
+def is_pair_symbol(sequence: str) -> bool:
+    return bool(sequence in pair_symbol_list)
 
 @dataclass
 class Token:
@@ -196,13 +261,30 @@ class Tokenizer():
     
     def __str__(self) -> str:
 
-        builder: str = ""
+        intermediate = []
 
         for token in self.tokens:
-            builder += str(token)
+            intermediate.append(str(token))
 
-        return builder
+        return ", \n".join(intermediate)
     
+    def test(self, expected_tokens):
+
+        equal = True
+
+        for i in range(0, len(self.tokens)):
+
+            print(self.tokens[i], end=" ")
+
+            if i < len(expected_tokens) and self.tokens[i] != expected_tokens[i]:
+                print(f"!= {expected_tokens[i]}")
+                equal = False
+            else:
+                print()
+
+        print()
+        print("equal") if equal else print("not equal")
+
     def setup(self):
 
         # Open the source file
@@ -239,27 +321,39 @@ class Tokenizer():
         return characters
     
     def parse_next(self) -> str:
-        buffer: str = ''
-        while self.peek() and self.peek().isalnum():
-            buffer += self.pop()
+
+        buffer: str = ""
+
+        # If there is a character to read
+        if peek := self.peek():
+
+            # If this character is a space, consume it
+            if peek.isspace():
+                self.pop()
+
+            # If this character is alphanumeric
+            elif peek.isalnum():
+
+                # While this character is alphanumeric, 
+                # build a buffer out of it.
+                while self.peek().isalnum():
+                    buffer += self.pop()
+
+            # If this character is not alphanumeric
+            elif not peek.isalnum():
+
+                if is_symbol(peek):
+                    buffer += self.pop()
+                else:
+                    # TODO: Make this a real error!
+                    print(f"ERROR: '{peek}' not identifiable!")
+                    self.pop()
+
         return buffer
-    
+
+    # TODO: Use later
     def process_newline(self, buffer: str) -> int:
         return self.line_count + 1 if buffer == "\n" else self.line_count
-    
-    # TODO: This doesn't need to be in this class. Static maybe? 
-    def tokenize_keywords(self, buffer: str) -> Optional[TokenType]:
-        return keywordDictionary[buffer] if buffer in keywordDictionary else None
-
-    def parse_operators(self) -> Optional[str]:
-        """Parses an operator at the current parsing head. Returns None on failure."""
-        
-        for N in [3,2,1]:
-            peek = self.peek(N)
-            if peek in operatorDictionary:
-                return peek
-
-        return None
     
     def tokenize(self) -> Result[None, str]:
         
@@ -269,36 +363,119 @@ class Tokenizer():
         # While there are characters to read
         while self.peek() is not None:
 
-            # Collect alphanumeric characters into a buffer
-            while self.peek().isalnum():
-                buffer += self.pop()
-
-            # When a non-alphanumeric character is 
-            # found, the identifier is complete
-
-            if keyword := self.tokenize_keywords(buffer):
-                self.tokens.append(keyword)
-            else:
-                self.tokens.append(Token(TokenType.IDENTIFIER, None, buffer))
-
+            buffer = self.parse_next()
             pass
+
+            # DEBUG:
+            if buffer == "*":
+                pass
+
+            if buffer.isspace():
+                continue
+
+            # Is the buffer a single quote?
+            elif buffer == "\'":
+                
+                full_char_sequence = "\'" + self.peek(2)
+
+                if is_literal_char(full_char_sequence):
+
+                    # Remove the quotes
+                    buffer = full_char_sequence.replace("\'", "", 2)
+                    self.tokens.append(Token(TokenType.LITERAL, None, buffer))
+
+
+            # Is the buffer a double quote?
+            elif buffer == "\"":
+
+                # Find the right hand double quote
+                while self.peek() != "\"":
+
+                    # Consume everything
+                    buffer += self.pop()
+
+                # Remove the first character (which should be a double quote)
+                buffer = buffer[1:-1]
+
+                self.tokens.append(Token(TokenType.LITERAL, None, buffer))
+
+            # Is the buffer a valid symbol?
+            elif token_type := is_symbol(buffer):
+                self.tokens.append(Token(token_type, None, None))
+
+            # Is the buffer a valid keyword?
+            elif token_type := is_keyword(buffer):
+                self.tokens.append(Token(token_type, None, None))
+
+            # Is the buffer a valid identifier?
+            elif token_type := is_identifier(buffer):
+                self.tokens.append(Token(token_type, None, buffer))
+
+            # Is the buffer a valid literal?
+            elif token_type := is_literal(buffer):
+                self.tokens.append(Token(token_type, None, buffer))
+
+            # Otherwise, ...?
+            else:
+
+                pass
         
         # Add an EOF to the end
-        self.tokens.append(Token(TokenType.EOF, self.source_file_contents_num_lines, None))
+        self.tokens.append(Token(TokenType.EOF, None, None))
 
         return Ok(self.tokens)
 
 if __name__ == "__main__":
 
     source_file_dir = "C:\\Users\\Orion\\Stash\\PersonalProjects\\Aries\\Compiler\\C Compiler (x86 Target)\\Source Files"
-    source_file_name = "ReturnZero.c"
+    # source_file_name = "ReturnZero.c"
+    source_file_name = "ReturnLiteralString.c"
     source_file_path = source_file_dir + "\\" + source_file_name
+
+    expected_token_list_return_zero = [
+        Token(TokenType.KEYWORD_INT, None, None),
+        Token(TokenType.IDENTIFIER, None, "main"),
+        Token(TokenType.SYM_PAREN_OPEN, None, None),
+        Token(TokenType.KEYWORD_INT, None, None),
+        Token(TokenType.IDENTIFIER, None, "argc"),
+        Token(TokenType.SYM_COMMA, None, None),
+        Token(TokenType.KEYWORD_CHAR, None, None),
+        Token(TokenType.SYM_STAR, None, None),
+        Token(TokenType.SYM_STAR, None, None),
+        Token(TokenType.IDENTIFIER, None, "argv"),
+        Token(TokenType.SYM_PAREN_CLOSE, None, None),
+        Token(TokenType.SYM_BRACE_OPEN, None, None),
+        Token(TokenType.KEYWORD_RETURN, None, None),
+        Token(TokenType.LITERAL, None, "0"),
+        Token(TokenType.SYM_SEMICOLON, None, None),
+        Token(TokenType.SYM_BRACE_CLOSE, None, None),
+        Token(TokenType.EOF, None, None),
+    ]
+
+    expected_token_list_return_literal_string = [
+        Token(TokenType.KEYWORD_CHAR, None, None),
+        Token(TokenType.SYM_STAR, None, None),
+        Token(TokenType.IDENTIFIER, None, "test"),
+        Token(TokenType.SYM_PAREN_OPEN, None, None),
+        Token(TokenType.SYM_PAREN_CLOSE, None, None),
+        Token(TokenType.SYM_BRACE_OPEN, None, None),
+        Token(TokenType.KEYWORD_RETURN, None, None),
+        Token(TokenType.LITERAL, None, "test"),
+        Token(TokenType.LITERAL, None, "string"),
+        Token(TokenType.LITERAL, None, "@"),
+        Token(TokenType.LITERAL, None, "$#**()}!"),
+        Token(TokenType.SYM_SEMICOLON, None, None),
+        Token(TokenType.SYM_BRACE_CLOSE, None, None),
+        Token(TokenType.EOF, None, None),
+    ]
 
     t = Tokenizer(source_file_path)
     t.setup()
     t.tokenize()
-
-    print(t)
+    
+    print()
+    
+    t.test(expected_token_list_return_literal_string)
 
 
 
