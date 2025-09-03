cd21c3862d265e8d74a3a0529bb0daee2678f987
Orion Cummings <orioncummings23@gmail.com>
2025-09-02 23:40:49 -0400

    adding generic types for use in compiler

---
+++ Compiler/C Rewrite/out/build/Default/src/CMakeFiles/AriesCompiler.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/orion/Projects/Aries/Compiler/C Rewrite/src/arena.c" "src/CMakeFiles/AriesCompiler.dir/arena.c.o" "gcc" "src/CMakeFiles/AriesCompiler.dir/arena.c.o.d"
+  "/home/orion/Projects/Aries/Compiler/C Rewrite/src/debug.c" "src/CMakeFiles/AriesCompiler.dir/debug.c.o" "gcc" "src/CMakeFiles/AriesCompiler.dir/debug.c.o.d"
+  "/home/orion/Projects/Aries/Compiler/C Rewrite/src/list.c" "src/CMakeFiles/AriesCompiler.dir/list.c.o" "gcc" "src/CMakeFiles/AriesCompiler.dir/list.c.o.d"
+  "/home/orion/Projects/Aries/Compiler/C Rewrite/src/main.c" "src/CMakeFiles/AriesCompiler.dir/main.c.o" "gcc" "src/CMakeFiles/AriesCompiler.dir/main.c.o.d"
+  "" "src/AriesCompiler" "gcc" "src/CMakeFiles/AriesCompiler.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
