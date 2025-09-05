18e2bc77e654ec034f550ac97b41d4cf1a69eddc
Orion Cummings <orioncummings23@gmail.com>
2025-09-04 20:52:18 -0400

    temp (working) state of the cmake restructure

---
+++ Compiler/C Rewrite/build/Release/src/CMakeFiles/AriesCompiler.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AriesCompiler.dir/link.d"
+  "AriesCompiler"
+  "AriesCompiler.pdb"
+  "CMakeFiles/AriesCompiler.dir/main.c.o"
+  "CMakeFiles/AriesCompiler.dir/main.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/AriesCompiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
